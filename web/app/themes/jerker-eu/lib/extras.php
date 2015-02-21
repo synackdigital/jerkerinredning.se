637e869d9631cd8be1feec28496b97e79afd89a0
Fredrik Broman <frebro@gmail.com>
2015-02-21 09:20:11 +0100

    upgraded roots to 8.0.0-446a35573e897128b735e52f4b11e5d755e9c063

--- web/app/themes/jerker-eu/lib/extras.php
+++ web/app/themes/jerker-eu/lib/extras.php
@@ -1,22 +1,33 @@
 <?php
+
+namespace Roots\Sage\Extras;
+
+use Roots\Sage\Config;
+
+/**
+ * Add <body> classes
+ */
+function body_class($classes) {
+  // Add page slug if it doesn't exist
+  if (is_single() || is_page() && !is_front_page()) {
+    if (!in_array(basename(get_permalink()), $classes)) {
+      $classes[] = basename(get_permalink());
+    }
+  }
+
+  // Add class if sidebar is active
+  if (Config\display_sidebar()) {
+    $classes[] = 'sidebar-primary';
+  }
+
+  return $classes;
+}
+add_filter('body_class', __NAMESPACE__ . '\\body_class');
+
 /**
  * Clean up the_excerpt()
  */
-function roots_excerpt_more($more) {
-  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'roots') . '</a>';
+function excerpt_more() {
+  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
-add_filter('excerpt_more', 'roots_excerpt_more');
-
-/**
- * Manage output of wp_title()
- */
-function roots_wp_title($title) {
-  if (is_feed()) {
-    return $title;
-  }
-
-  $title .= get_bloginfo('name');
-
-  return $title;
-}
-add_filter('wp_title', 'roots_wp_title', 10);
+add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
