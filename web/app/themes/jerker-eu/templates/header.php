5bec69aeea995cc11e6b0d2613976ed51fbacbc2
Fredrik Broman <frebro@gmail.com>
2015-04-27 08:43:50 +0200

    navbar layout and logo graphic

--- web/app/themes/jerker-eu/templates/header.php
+++ web/app/themes/jerker-eu/templates/header.php
@@ -9,10 +9,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+        <?php include(get_stylesheet_directory().'/assets/images/bird.svg'); ?>
+        <?php bloginfo('name'); ?>
+      </a>
     </div>
 
-    <nav class="collapse navbar-collapse" role="navigation">
+    <nav class="collapse navbar-collapse navbar-right" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new Nav\SageNavWalker(), 'menu_class' => 'nav navbar-nav']);
