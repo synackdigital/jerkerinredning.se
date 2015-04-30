aa4f6eff2de8783f15e1087bfa3a36046b9e32e9
Fredrik Broman <frebro@gmail.com>
2015-04-30 10:58:36 +0200

    acf text/image module tweaks

--- web/app/themes/jerker-eu/templates/acf-text_image.php
+++ web/app/themes/jerker-eu/templates/acf-text_image.php
@@ -2,26 +2,26 @@
 
 $repeater_count = count(get_sub_field('repeater'));
 
-$column_classes = ['col-xs-12'];
+$column_classes = array('col-xs-12');
 
 switch ($repeater_count) :
-  case 2: $column_classes[] = 'col-sm-6'; break;
-  case 3: $column_classes[] = 'col-sm-4'; break;
-  case 4: $column_classes[] = 'col-sm-6 col-lg-3'; break;
+  case 2: array_push($column_classes, 'col-sm-6', 'col-md-6', 'col-lg-6'); break;
+  case 3: array_push($column_classes, 'col-sm-4', 'col-md-4', 'col-lg-4'); break;
+  case 4: array_push($column_classes, 'col-sm-6', 'col-md-6', 'col-lg-6', 'col-lg-3'); break;
 endswitch;
 
-if ( have_rows('repeater') ): ?>
+if ( $repeater_count > 0 ): ?>
 
-<section class="container acf acf-text-image">
+<section class="<?php the_sub_field('container'); ?> acf acf-text-image">
   <div class="row">
-  <?php while ( have_rows('repeater') ) : the_row(); ?>
+    <?php if ( get_sub_field('headline')) : ?><div class="col-xs-12"><h1 class="acf-section-headline"><?php the_sub_field('headline'); ?></h1></div><?php endif; ?>
+    <?php if ( have_rows('repeater') ) : while ( have_rows('repeater') ) : the_row(); ?>
     <div class="<?php echo implode(' ', $column_classes); ?>">
-      <?php $image = get_sub_field('image'); ?>
-      <img class="acf-image img-responsive" src="<?php echo $image['url'] ?>" alt="">
-      <h1 class="acf-headline"><?php the_sub_field('headline'); ?></h1>
-      <div class="acf-text"><?php the_sub_field('text'); ?></div>
+      <?php if ( get_sub_field('image')) : ?><img class="acf-image img-responsive" src="<?php the_sub_field('image'); ?>"><?php endif; ?>
+      <?php if ( get_sub_field('headline')) : ?><h2 class="acf-headline"><?php the_sub_field('headline'); ?></h2><?php endif; ?>
+      <?php if ( get_sub_field('text')) : ?><div class="acf-text"><?php the_sub_field('text'); ?></div><?php endif; ?>
     </div>
-  <?php endwhile; ?>
+    <?php endwhile; endif; ?>
   </div>
 </section>
 
