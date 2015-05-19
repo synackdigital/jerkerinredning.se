29b1f1d36c90a0a23785a32eb1ac8d2223547ca9
Fredrik Broman <frebro@gmail.com>
2015-05-19 17:58:16 +0200

    grid and thumbnail tweaks

--- web/app/themes/jerker-eu/templates/page_section-teasers.php
+++ web/app/themes/jerker-eu/templates/page_section-teasers.php
@@ -7,7 +7,7 @@ $teasers_count = count( $teasers );
 // Array of responsive column classes
 $column_classes = array('col-xs-12');
 switch ($teasers_count) :
-  case 1 : array_push($column_classes, 'col-sm-12', 'col-md-9', 'col-md-offset-6', 'col-lg-9', 'col-lg-offset-6'); break;
+  case 1 : array_push($column_classes, 'col-sm-8 col-sm-offset-2', 'col-md-6', 'col-md-offset-3', 'col-lg-4', 'col-lg-offset-4'); break;
   case 2 : array_push($column_classes, 'col-sm-6', 'col-md-6', 'col-lg-6'); break;
   case 3 : array_push($column_classes, 'col-sm-4', 'col-md-4', 'col-lg-4'); break;
   case 4 : array_push($column_classes, 'col-sm-6', 'col-md-6', 'col-lg-6', 'col-lg-3'); break;
@@ -29,11 +29,15 @@ if ( $teasers_count > 0 ) : ?>
           $link_label = get_sub_field('link_label');
       ?>
       <div class="<?= implode(' ', $column_classes); ?>">
-        <?php if ( !empty( $image ) ) : ?><img class="page-section-image" src="<?= $image['sizes']['medium']; ?>" alt="<?= $image['alt']; ?>"><?php endif; ?>
-        <?php if ( !empty( $headline ) ) : ?><h1 class="page-section-headline"><span class="magic-underline"><?= $headline; ?></span></h1><?php endif; ?>
-        <?php if ( !empty( $subhead ) ) : ?><h2 class="page-section-subhead"><?= $subhead; ?></h2><?php endif; ?>
-        <?php if ( !empty( $text ) ) : ?><div class="page-section-text"><?= $text; ?></div><?php endif; ?>
-        <?php if ( !empty( $link_url ) && !empty( $link_label ) ) : ?><a class="page-section-link" href="<?= $link_url; ?>"><?= $link_label; ?></a><?php endif; ?>
+        <div class="thumbnail">
+          <?php if ( !empty( $image ) ) : ?><img src="<?= $image['sizes']['medium']; ?>" alt="<?= $image['alt']; ?>"><?php endif; ?>
+          <div class="caption">
+            <?php if ( !empty( $headline ) ) : ?><h1><span class="magic-underline"><?= $headline; ?></span></h1><?php endif; ?>
+            <?php if ( !empty( $subhead ) ) : ?><h2><?= $subhead; ?></h2><?php endif; ?>
+            <?php if ( !empty( $text ) ) : ?><?= $text; ?><?php endif; ?>
+            <?php if ( !empty( $link_url ) && !empty( $link_label ) ) : ?><a class="page-section-link" href="<?= $link_url; ?>"><?= $link_label; ?></a><?php endif; ?>
+          </div>
+        </div>
       </div>
       <?php endwhile; endif; ?>
     </div>
