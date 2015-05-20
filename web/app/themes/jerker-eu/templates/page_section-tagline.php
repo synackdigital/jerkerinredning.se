affcb8f933c7df36b4906aeebe044e653b2c84d4
Fredrik Broman <frebro@gmail.com>
2015-05-20 10:32:18 +0200

    added basic tagline section template

---
+++ web/app/themes/jerker-eu/templates/page_section-tagline.php
@@ -0,0 +1,17 @@
+<?php
+
+$headline = get_sub_field('headline');
+
+?>
+
+<section class="page-section page-section-tagline">
+  <div class="container">
+    <div class="row" style="margin-top:120px;margin-bottom:120px;text-align:center;">
+      <div class="col-xs-12 col-md-10 col-md-offset-1">
+        <div class="page-section-tiny_mce-content">
+          <h1 class="font-serif-xlarge"><?= $headline ?></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
