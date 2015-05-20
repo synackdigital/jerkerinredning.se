ca525a6a18e0bb8e058042b1fddecf20965b4df5
Fredrik Broman <frebro@gmail.com>
2015-05-20 10:30:45 +0200

    post thumbnail used as background instead of img

--- web/app/themes/jerker-eu/templates/page-header.php
+++ web/app/themes/jerker-eu/templates/page-header.php
@@ -1,13 +1,23 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header">
+<?php
+
+if ( has_post_thumbnail() ) :
+  $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+  $background_image = $thumbnail['0'];
+else :
+  $background_image = 'bar';
+endif;
+
+?>
+
+<div class="page-header" style="background-image:url(<?php echo $background_image; ?>);">
   <?php
   if (have_rows('hero')) :
     while (have_rows('hero')) : the_row();
       get_template_part('templates/hero', get_row_layout());
     endwhile;
   else : ?>
-  <?php if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
   <div class="page-title">
     <div class="container">
       <h1 class="font-serif-xxlarge">
