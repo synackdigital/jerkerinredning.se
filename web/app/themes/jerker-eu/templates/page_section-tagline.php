a6098ef9d0aff08b177fd050f03d011862b558c0
Fredrik Broman <frebro@gmail.com>
2015-05-20 14:02:05 +0200

    tagline section styling

--- web/app/themes/jerker-eu/templates/page_section-tagline.php
+++ web/app/themes/jerker-eu/templates/page_section-tagline.php
@@ -6,7 +6,7 @@ $headline = get_sub_field('headline');
 
 <section class="page-section page-section-tagline">
   <div class="container">
-    <div class="row" style="margin-top:120px;margin-bottom:120px;text-align:center;">
+    <div class="row" style="margin-top:120px;margin-bottom:40px;text-align:center;">
       <div class="col-xs-12 col-md-10 col-md-offset-1">
         <div class="page-section-tiny_mce-content">
           <h1 class="font-serif-xlarge"><?= $headline ?></h1>
