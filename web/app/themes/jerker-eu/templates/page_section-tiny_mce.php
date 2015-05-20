5ba9a75551755854253fdffa78c5f0143bfdb553
Fredrik Broman <frebro@gmail.com>
2015-05-20 09:23:09 +0200

    added tiny_mce section template

---
+++ web/app/themes/jerker-eu/templates/page_section-tiny_mce.php
@@ -0,0 +1,15 @@
+<?php
+
+$tinymce = get_sub_field('tinymce');
+
+?>
+
+<section class="page-section page-section-tiny_mce">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-md-6">
+        <div class="page-section-tiny_mce-content"><?= $tinymce ?></div>
+      </div>
+    </div>
+  </div>
+</section>
