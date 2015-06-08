47217020c6bee4963327b02747e3a2089b950029
Fredrik Broman <frebro@gmail.com>
2015-06-08 16:32:59 +0200

    broke out instafeed js in a separate file

--- web/app/themes/jerkerinredning/assets/scripts/main.js
+++ web/app/themes/jerkerinredning/assets/scripts/main.js
@@ -23,48 +23,10 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
+        initializeInstafeed();
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
-
-        // Find and initialize Instafeed canvases
-        $('.instafeed-canvas').each(function(i) {
-          $this = $(this);
-
-          // Assign a unique ID for this container
-          uniqid = Date.now();
-          $this.attr('id', 'instafeed-canvas-'+uniqid);
-
-          // Retrieve data from the data-instafeed attribute
-          $instafeed_data = $this.data('instafeed');
-
-          // Create the Instafeed
-          var instafeed = new Instafeed({
-
-            // Fetch credentials from PHPVAR
-            clientId: PHPVAR.instagram_client_id,
-            userId: parseInt(PHPVAR.instagram_user_id),
-            accessToken: PHPVAR.instagram_access_token,
-
-            // Create attribues
-            target: $this.attr('id'),
-            get: $instafeed_data.get,
-            limit: parseInt($instafeed_data.limit),
-            resolution: $instafeed_data.resolution,
-            links: $instafeed_data.links,
-
-            // HTML Template
-            template: '<div class="col-xs-12 col-sm-4 col-lg-3"><div class="thumbnail"><a href="{{link}}"><img src="{{image}}" alt="{{caption}}"></a></div></div>',
-
-            // Handle callbacks
-            before: function() {},
-            success: function() {},
-            after: function() {},
-            error: function() {}
-          });
-          instafeed.run();
-        });
-
       }
     },
     // Home page
