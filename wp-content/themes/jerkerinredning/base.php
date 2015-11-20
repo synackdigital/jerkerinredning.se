8dc8d5b6f7b1eaf738e49ecb00531ec562023da1
Fredrik Broman <frebro@gmail.com>
2015-11-20 16:27:52 +0100

    Remove Browsehappy message

--- wp-content/themes/jerkerinredning/base.php
+++ wp-content/themes/jerkerinredning/base.php
@@ -9,11 +9,6 @@ use Roots\Sage\Wrapper;
 
 <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
-    <!--[if lt IE 9]>
-      <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
-      </div>
-    <![endif]-->
     <div class="wrap" role="document">
       <?php include Wrapper\template_path(); ?>
     </div><!-- /.wrap -->
