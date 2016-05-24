2610f42f269b2c150e0fec588a5e89882b5a3686
Fredrik Broman <frebro@gmail.com>
2016-05-24 21:36:30 +0200

    Increase post-thumbnail max size

--- wp-content/themes/jerkerinredning/templates/post-thumbnail.php
+++ wp-content/themes/jerkerinredning/templates/post-thumbnail.php
@@ -1,2 +1,2 @@
-<?php $post_thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id(),array(1680, 945)); ?>
+<?php $post_thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id(), 1920); ?>
 <div class="post-thumbnail" style="background-image:url('<?php echo $post_thumbnail[0]; ?>');">&nbsp;</div>
