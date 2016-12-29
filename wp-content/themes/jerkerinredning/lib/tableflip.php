8cfa6cbbe28a53492e94028534d14fcc69080d98
Fredrik Broman <frebro@gmail.com>
2016-12-29 23:34:27 +0100

    Close #21 - Add customer to email send list if email address is supplied

--- wp-content/themes/jerkerinredning/lib/tableflip.php
+++ wp-content/themes/jerkerinredning/lib/tableflip.php
@@ -2,41 +2,64 @@
 
 namespace Jerker\Tableflip;
 
+// Enable email through AJAX
 add_action( 'wp_ajax_mail_tableflip_order', __NAMESPACE__.'\\mail_tableflip_order' );
 add_action( 'wp_ajax_nopriv_mail_tableflip_order', __NAMESPACE__.'\\mail_tableflip_order' );
 
 function mail_tableflip_order() {
+
+  // Continue only if data is recieved
   if (isset($_POST['data'])) {
 
+    // Turn recieved data into JSON
     $data = json_decode(stripslashes($_POST['data']), true);
 
     // Get recipient email address from ACF options page, or fallback to admin email
+    $to = array();
     if (function_exists('get_field') && get_field('order_recipient_email', 'option')):
-      $to = get_field('order_recipient_email', 'option');
+      $to[] = get_field('order_recipient_email', 'option');
     else:
-      $to = get_option('admin_email');
+      $to[] = get_option('admin_email');
     endif;
 
-    $subject = 'En beställning från jerkerinredning.se';
-    $message = ''
-      . 'En beställning skickades från <strong>'.$data['customer']['name'].'</strong><br>'
-      . 'Telefon: '.$data['customer']['phone'].'<br>'
-      . 'E-post: '.$data['customer']['email'].'<br><br>'
-      . '<strong>'.$data['order']['model']['name'].'</strong>, '
-      . $data['order']['material']['name'].' med '
-      . $data['order']['finish']['name'].'<br><br>'
-      . 'Kundens meddelande: "' . $data['customer']['message'].'"<br><br>'
-      . 'Bredd/längd: '.$data['order']['width'].'/'.$data['order']['length'].'<br>'
-      . 'Angivet pris: '.$data['order']['price'].' '.$data['order']['currency'];
+    // Create email subject and body
+    $subject = 'En offertförfrågan från jerkerinredning.se';
 
+    $message = array();
+    $message[] = 'En offertförfrågan skickades från <strong>'.$data['customer']['name'].'</strong> till '.implode('', $to).'.';
+
+    if ($data['customer']['email'])
+      $message[] = 'E-post: '.$data['customer']['email'];
+
+    if ($data['customer']['phone'])
+      $message[] = 'Telefon: '.$data['customer']['phone'];
+
+    $message[] = '<strong>'.$data['order']['model']['name'].', '
+                  . strtolower($data['order']['material']['name']).' med '
+                  . strtolower($data['order']['finish']['name']).'</strong><br /><br />'
+                  . 'Bredd/längd: '.$data['order']['width'].'/'.$data['order']['length'].'<br /><br />'
+                  . 'Uppskattat pris: '.$data['order']['price'].' '.$data['order']['currency'];
+
+    if ($data['customer']['message'])
+      $message[] = 'Meddelande: "'.$data['customer']['message'].'"';
+
+    // Add customer to send list
+    if ($data['customer']['email'])
+      $to[] = $data['customer']['email'];
+
+    // Switch to HTML content type for wp_mail()
     add_filter('wp_mail_content_type', 'set_html_content_type');
 
-    $mail = wp_mail($to, $subject, $message);
+    // Send order through wp_mail()
+    $mail = wp_mail($to, $subject, implode('<br /><br />', $message));
 
+    // Switch back to default content type
     remove_filter('wp_mail_content_type', 'set_html_content_type');
 
     echo $_POST['data'];
   }
+
+  // Return an error if no data was recieved
   else {
     echo '$_POST is not set';
   }
