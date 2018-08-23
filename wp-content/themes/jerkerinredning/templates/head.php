6ddc45ddd6f351873791288796b31b741de84d86
Fredrik Broman <frebro@gmail.com>
2018-08-23 21:46:07 +0200

    Add Pinterest meta to head

--- wp-content/themes/jerkerinredning/templates/head.php
+++ wp-content/themes/jerkerinredning/templates/head.php
@@ -5,5 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
+    <meta name="p:domain_verify" content="83dcd88972b7c7463e62f4ff68e966b4">
     <?php wp_head(); ?>
   </head>
